59cafbeda5b5879976f900bed6aacd27afcab381
Warren <warrenwang2000@gmail.com>
2025-02-20 13:05:20 +0100

    update the function

--- codecamp/__init__.py
+++ codecamp/__init__.py
@@ -2,21 +2,21 @@
 """
 import numpy as np
 
-def load_resp(path_resp,t_start=0):
-    data = np.loadtxt(path_resp,skiprows=1)
-    time_column = data[:,0] # Get the time column
-    start_index = np.where(time_column >= t_start)[0][0]  # Get first occurrence
-    data_filter = data[start_index:,:]
+def load_resp(path_resp,t_start=60):
+    data = np.loadtxt(path_resp,skiprows=1) # load the data file
+    time_column = data[:,0] # get the time column
+    start_index = np.where(time_column >= t_start)[0][0]  # get the first occurrence
+    data_filter = data[start_index:,:] # filter the data
     t = data_filter[:,0]
     u = data_filter[:,1]
     xb = data_filter[:,2]
     xt = data_filter[:,3]
     return [t,u,xb,xt]
 
-def load_wind(path_wind,t_start=0):
-    data = np.loadtxt(path_wind,skiprows=1)
-    time_column = data[:,0] # Get the time column
-    start_index = np.where(time_column >= t_start)[0][0]  # Get first occurrence
+def load_wind(path_wind,t_start=60):
+    data = np.loadtxt(path_wind,skiprows=1) # load the data file
+    time_column = data[:,0] # get the time column
+    start_index = np.where(time_column >= t_start)[0][0]  # get the first occurrence
     data_filter = data[start_index:,:]
     t_wind = data_filter[:,0]
     u_wind = data_filter[:,1]
